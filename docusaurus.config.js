1a601e2cb58785199c10c244c0122e4e32928dc6
ncoop57 <nathan.cooper@stability.ai>
2024-03-16 18:14:00 -0500

    Update site title and logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Code.AI',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Code.AI Logo',
           src: 'img/logo.svg',
         },
         items: [
