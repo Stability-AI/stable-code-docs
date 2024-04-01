41a751fcceaf4e4312ad48188fdc0cb45be66497
ncoop57 <nacooper01@email.wm.edu>
2024-04-01 16:12:42 -0500

    Corregir URL y base URL en docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://stable-code-docs.github.io/',
+  url: 'https://stability-ai.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/stable-code-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
